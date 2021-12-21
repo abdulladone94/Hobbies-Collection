220550427c6ecdc1ca411d0e7cf932fb906c161e
abdulladone94 <abdulladone94@gmail.com>
2021-12-21 17:49:05 +0530

    Create mysql login schema and connecting

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 require("dotenv").config();
 const express = require("express");
+var mysql = require("mysql");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const PORT = 3003;
 
 const app = express();
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(cors());
 
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "hobbies_collection",
+});
+
 app.get("/", (req, res) => {
-  res.send("home page hellow  mfdn ");
+  res.send("Hello WORLD");
 });
 
 app.listen(PORT, () => {
