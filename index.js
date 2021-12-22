e1d5633dd5ad60ab05554d7d6516fe1fa277df21
abdulladone94 <abdulladone94@gmail.com>
2021-12-22 07:53:13 +0530

    Check availability of alredy registered users

--- index.js
+++ index.js
@@ -30,6 +30,26 @@ app.post("/register", (req, res) => {
   );
 });
 
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  db.query(
+    "SELECT * FROM login WHERE username = ? and password = ?",
+    [username, password],
+    (err, result) => {
+      if (err) {
+        res.send({ err: err });
+      }
+      if (result.length > 0) {
+        res.send(result);
+      } else {
+        res.send({ message: "Wrong entey" });
+      }
+    }
+  );
+});
+
 app.get("/", (req, res) => {
   res.send("home page hellow  mfdn ");
 });
